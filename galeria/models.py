3d51a58b2d05644c7bc0e132f404a29ba7c96afc
Caio Sella <sellacaio4@gmail.com>
2023-04-22 18:04:18 -0300

    Adcionando campo para inserir imagens no admin

--- galeria/models.py
+++ galeria/models.py
@@ -17,7 +17,7 @@ class Fotografia(models.Model):
     categoria = models.CharField(
         max_length=100, choices=OPCOES_CATEGORIA,  default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
