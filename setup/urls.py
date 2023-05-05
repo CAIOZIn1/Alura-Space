f5f747e423248e40d3dd44b5e23b8e59afae1751
Caio Sella <sellacaio4@gmail.com>
2023-05-04 23:47:49 -0300

    Template login e cadastro finalizados

--- setup/urls.py
+++ setup/urls.py
@@ -6,4 +6,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
+    path('', include('usuarios.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ usuarios/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def login(request):
+    return (render(request, 'usuarios/login.html'))
+
+
+def cadastro(request):
+    return (render(request, 'usuarios/cadastro.html'))
