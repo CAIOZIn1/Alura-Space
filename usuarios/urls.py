b563b2de70047f8b08518a1a3296935d155a51d2
Caio Sella <sellacaio4@gmail.com>
2023-05-06 18:22:16 -0300

    Implementado sistema de login funcional

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from usuarios.views import cadastro, login
+from usuarios.views import cadastro, login, logout
 
 urlpatterns = [
     path('login', login, name='login'),
-    path('cadastro', cadastro, name='cadastro')
+    path('cadastro', cadastro, name='cadastro'),
+    path('logout', logout, name='logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,73 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
 from usuarios.forms import CadastroForms, LoginForms
 
 
 def login(request):
     form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+
+        if usuario is not None:
+            auth.login(request, usuario)
+            messages.success(request, f'{nome} logado com sucesso!')
+            return redirect('index')
+
+        messages.error(
+            request, 'Erro no login')
+        return redirect('login')
+
     return render(request, 'usuarios/login.html', {'form': form})
 
 
 def cadastro(request):
     form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            if form['senha1'].value() != form['senha2'].value():
+                messages.error(request, 'Senhas diferentes entre si.')
+                return redirect('cadastro')
+
+            nome = form['nome_cadastro'].value()
+            email = form['email'].value()
+            senha = form['senha1'].value()
+
+            if User.objects.filter(username=nome).exists():
+                messages.error(
+                    request, 'User com este nome já existe, tente outro.')
+                return redirect('cadastro')
+
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+
+            usuario.save()
+            messages.success(
+                request, f'User {nome} foi cadastrado com sucesso!')
+            return redirect('login')
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'logout efetuado com sucesso!')
+    return redirect('login')
